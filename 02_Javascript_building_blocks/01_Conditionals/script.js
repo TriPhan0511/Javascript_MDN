e59cdf5f6175c9c1e359e0cd4e56d234f2f50c13
Tri Phan <phanphutri@gmail.com>
2021-05-06 18:17:43 +0700

    Added 02_Javascript_building_blocks

---
+++ 02_Javascript_building_blocks/01_Conditionals/script.js
@@ -0,0 +1,24 @@
+/**
+ * 1. YOU CAN HAVE IT ON ONE CONDITION..!
+ */
+
+/**
+ * 2. if..else statements
+ */
+
+// let shoppingDone = false;
+// let childsAllowance;
+
+// if (shoppingDone) {
+//   childsAllowance = 10;
+// } else {
+//   childsAllowance = 5;
+// }
+
+// console.log(childsAllowance); // 5
+// ----------------------------------------------------------------------------------------
+
+/**
+ * 3. else if
+ */
+
